c19c56a2bb99b34864b80c05a56179437e485184
thongguma0907-wq <thongguma0907@gmail.com>
2026-06-23 05:48:36 +0700

    Add NPC Shop System

---
+++ Assets/UI Shop/ShopItemUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopItemUI : MonoBehaviour
+{
+    public Image itemImage;
+    public TMP_Text priceText;
+    public Button buyButton;
+
+    private ShopItemData itemData;
+
+    public void Setup(ShopItemData data)
+    {
+        itemData = data;
+
+        itemImage.sprite = data.itemImage;
+        priceText.text = data.price.ToString() + " Gold";
+
+        buyButton.onClick.RemoveAllListeners();
+        buyButton.onClick.AddListener(BuyItem);
+    }
+
+    void BuyItem()
+    {
+        Debug.Log("Đã mua: " + itemData.itemName);
+
+        // Sau này trừ tiền ở đây
+        // PlayerGold -= itemData.price;
+    }
+}
