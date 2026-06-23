7da706390c60e646701a4925bf6bed1b572b3276
thongguma0907-wq <thongguma0907@gmail.com>
2026-06-24 04:10:31 +0700

    Chinh Sua UIShop

--- Assets/UI Shop/NPCShop.cs
+++ Assets/UI Shop/NPCShop.cs
@@ -1,41 +1,129 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 public class NPCShop : MonoBehaviour
 {
-    public GameObject fText;
-    public ShopManager shopManager;
+    [Header("UI")]
+    [SerializeField] private GameObject pressFObject;
 
-    private bool playerInRange = false;
+    [Header("SHOP")]
+    [SerializeField] private ShopManager shopManager;
 
-    void Start()
+    private readonly HashSet<Collider2D> playerColliders =
+        new HashSet<Collider2D>();
+
+    private bool PlayerIsNear
     {
-        fText.SetActive(false);
+        get
+        {
+            return playerColliders.Count > 0;
+        }
     }
 
-    void Update()
+    private void Start()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        UpdatePrompt();
+    }
+
+    private void Update()
+    {
+        UpdatePrompt();
+
+        if (PlayerIsNear && InteractPressed())
         {
+            if (shopManager == null)
+            {
+                Debug.LogError(
+                    "NPCShop chưa được gán ShopManager."
+                );
+
+                return;
+            }
+
             shopManager.ToggleShop();
+            UpdatePrompt();
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
+        if (!IsPlayer(other))
         {
-            playerInRange = true;
-            fText.SetActive(true);
+            return;
         }
+
+        playerColliders.Add(other);
+        UpdatePrompt();
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
+        if (!IsPlayer(other))
         {
-            playerInRange = false;
-            fText.SetActive(false);
+            return;
+        }
+
+        playerColliders.Remove(other);
+
+        if (!PlayerIsNear && shopManager != null)
+        {
             shopManager.CloseShop();
+        }
+
+        UpdatePrompt();
+    }
+
+    private bool IsPlayer(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            return true;
+        }
+
+        Transform root = other.transform.root;
+
+        return root != null && root.CompareTag("Player");
+    }
+
+    private void UpdatePrompt()
+    {
+        if (pressFObject == null)
+        {
+            return;
+        }
+
+        bool shouldShow =
+            PlayerIsNear &&
+            shopManager != null &&
+            !shopManager.IsOpen;
+
+        if (pressFObject.activeSelf != shouldShow)
+        {
+            pressFObject.SetActive(shouldShow);
+        }
+    }
+
+    private bool InteractPressed()
+    {
+#if ENABLE_INPUT_SYSTEM
+        return Keyboard.current != null &&
+               Keyboard.current.fKey.wasPressedThisFrame;
+#else
+        return Input.GetKeyDown(KeyCode.F);
+#endif
+    }
+
+    private void OnDisable()
+    {
+        playerColliders.Clear();
+
+        if (pressFObject != null)
+        {
+            pressFObject.SetActive(false);
         }
     }
 }
