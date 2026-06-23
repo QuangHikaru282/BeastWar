837b3c95fa9da738ce55169760f76c3a03b60738
thongguma0907-wq <thongguma0907@gmail.com>
2026-06-24 03:08:07 +0700

    Backup truoc khi khoi phuc Canvas

--- Assets/UI Shop/ShopItemData.cs
+++ Assets/UI Shop/ShopItemData.cs
@@ -1,9 +1,45 @@
+﻿using System;
 using UnityEngine;
 
-[System.Serializable]
+public enum ShopItemCategory
+{
+    Weapon,
+    Armor,
+    Item
+}
+
+[Serializable]
 public class ShopItemData
 {
+    [Header("Thông tin cơ bản")]
     public string itemName;
-    public Sprite itemImage;
+
+    public Sprite icon;
+
+    public ShopItemCategory category;
+
+    [Min(0)]
     public int price;
+
+    [Tooltip("Ví dụ: Level 5 Weapon")]
+    public string itemTypeText;
+
+    [Header("Chỉ số")]
+    [Min(0)]
+    public int attack;
+
+    [Min(0)]
+    public int durability = 100;
+
+    [TextArea(2, 5)]
+    public string description;
+
+    [Header("Số lượng ban đầu")]
+    [Min(0)]
+    public int startingOwned;
+
+    // Số lượng trong lúc chơi game.
+    // Không hiện trong Inspector.
+    [NonSerialized]
+    public int owned;
 }
