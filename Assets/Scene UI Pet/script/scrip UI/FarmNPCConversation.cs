e3a2d0e5bb26483a17fdeaf1d8dc1ddc51f73eea
thongguma0907-wq <thongguma0907@gmail.com>
2026-08-14 23:30:50 +0700

    Update

---
+++ Assets/Scene UI Pet/script/scrip UI/FarmNPCConversation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FarmConversationSpeaker
+{
+    NPC,
+    Player
+}
+
+[Serializable]
+public class FarmConversationLine
+{
+    [Tooltip("Chọn nhân vật đang nói câu này.")]
+    public FarmConversationSpeaker speaker = FarmConversationSpeaker.NPC;
+
+    [TextArea(2, 5)]
+    [Tooltip("Nội dung sẽ hiển thị trong khung hội thoại.")]
+    public string text;
+}
+
+[RequireComponent(typeof(Collider2D))]
+public class FarmNPCConversation : MonoBehaviour
+{
+    [Header("Thông tin NPC")]
+    [SerializeField] private string npcName = "Tên NPC";
+    [SerializeField] private Sprite npcPortrait;
+
+    [Header("Nội dung hội thoại")]
+    [SerializeField] private List<FarmConversationLine> dialogueLines = new List<FarmConversationLine>();
+
+    public string NPCName => npcName;
+    public Sprite NPCPortrait => npcPortrait;
+    public IReadOnlyList<FarmConversationLine> DialogueLines => dialogueLines;
+
+    private void Reset()
+    {
+        Collider2D trigger = GetComponent<Collider2D>();
+        trigger.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (FarmConversationManager.Instance != null)
+            FarmConversationManager.Instance.SetNearbyNPC(this);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (FarmConversationManager.Instance != null)
+            FarmConversationManager.Instance.ClearNearbyNPC(this);
+    }
+
+    private void OnDisable()
+    {
+        if (FarmConversationManager.Instance != null)
+            FarmConversationManager.Instance.ClearNearbyNPC(this);
+    }
+}
---
+++ Assets/UI Shop/Frefab/SinglePortraitNPCConversation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SinglePortraitDialogueSpeaker
+{
+    NPC,
+    Player
+}
+
+[Serializable]
+public class SinglePortraitDialogueLine
+{
+    [Tooltip("Chọn NPC hoặc Player là người nói câu này.")]
+    public SinglePortraitDialogueSpeaker speaker = SinglePortraitDialogueSpeaker.NPC;
+
+    [TextArea(2, 5)]
+    [Tooltip("Nhập nội dung câu thoại.")]
+    public string text;
+}
+
+[RequireComponent(typeof(Collider2D))]
+public class SinglePortraitNPCConversation : MonoBehaviour
+{
+    [Header("THÔNG TIN NPC")]
+    [SerializeField] private string npcName = "Tên NPC";
+    [SerializeField] private Sprite npcPortrait;
+
+    [Header("CÁC CÂU HỘI THOẠI")]
+    [SerializeField]
+    private List<SinglePortraitDialogueLine> lines =
+        new List<SinglePortraitDialogueLine>();
+
+    public string NPCName => npcName;
+    public Sprite NPCPortrait => npcPortrait;
+    public IReadOnlyList<SinglePortraitDialogueLine> Lines => lines;
+
+    private void Reset()
+    {
+        Collider2D interactionArea = GetComponent<Collider2D>();
+        interactionArea.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (SinglePortraitDialogueManager.Instance != null)
+            SinglePortraitDialogueManager.Instance.RegisterNearbyNPC(this);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (SinglePortraitDialogueManager.Instance != null)
+            SinglePortraitDialogueManager.Instance.UnregisterNearbyNPC(this);
+    }
+
+    private void OnDisable()
+    {
+        if (SinglePortraitDialogueManager.Instance != null)
+            SinglePortraitDialogueManager.Instance.UnregisterNearbyNPC(this);
+    }
+}
